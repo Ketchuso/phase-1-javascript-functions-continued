ee815ff1f2fd2562ce0dc1fb0a6913b6741922a8
Jay Davis <jdlikesbsbl@gmail.com>
2025-01-28 11:08:15 -0600

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(words){
+     let activity = "roller-skate";
+     if (words){
+        activity = words;
+     }
+     return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function (words){
+    let activity = "go to the office";
+    if (words){
+        activity = words;
+    }
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair){
+    return function result(adj = "special"){
+        return `You are ${flair}${adj}${flair}!`;
+    }
+}
